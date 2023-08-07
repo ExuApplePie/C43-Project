4378005bf5e70446146bdd9e93fb5b8dd9e79540
ExuApplePie <admiral123456789@hotmail.com>
2023-08-07 15:23:34 -0400

    add amenities size > 0 check

--- src/main/java/org/Controller/QueriesController.java
+++ src/main/java/org/Controller/QueriesController.java
@@ -54,7 +54,7 @@ public class QueriesController {
 
     private static String generateAmenitiesQuery(List<String> amenities) {
         String amenitiesQuery = "";
-        if (amenities != null) {
+        if (amenities != null && amenities.size() > 0) {
             amenitiesQuery = "AND name IN (";
             for (int i = 0; i < amenities.size(); i++) {
                 amenitiesQuery += "'" + amenities.get(i) + "'";
@@ -84,8 +84,8 @@ public class QueriesController {
         createDatesWithAmenitiesView();
         String orderBy = orderByDistance ? "DISTANCE" : (orderByPriceAsc ? "price ASC" : "price DESC");
         String queryFilter = generateAllQueries(startDate, endDate, lowestPrice, highestPrice, amenities);
-        String query = "SELECT listingId FROM dates WHERE (6371 * acos(cos(radians(" + latitude + ")) " +
-                "* cos(radians(latitude)) * cos(radians(longitude) - radians(" + longitude + ")) + sin(radians(" + latitude + ")) * sin(radians(latitude))))" + " AS DISTANCE " +
+        String query = "SELECT listingId, (6371 * acos(cos(radians(" + latitude + ")) " +
+                "* cos(radians(latitude)) * cos(radians(longitude) - radians(" + longitude + ")) + sin(radians(" + latitude + ")) * sin(radians(latitude)))) AS DISTANCE " +
                 queryFilter + " FROM Listing HAVING DISTANCE < " + distance + " ORDER BY " + orderBy;
         return getDates(query);
     }
